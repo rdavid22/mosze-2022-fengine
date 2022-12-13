e372ba3a55dfa1be13be35724ce973bc25d005a9
gergohaszonits <haszonits.gergo@gmail.com>
2022-12-13 18:40:08 +0100

    add frame and answers objects

---
+++ engine/objects/answer.h
@@ -0,0 +1,42 @@
+#ifndef FENGINE_OBJECTS_ANSWER_H_
+#define FENGINE_OBJECTS_ANSWER_H_
+
+#include "iostream"
+#include <vector>
+
+class Answer
+{
+private:
+    uint16_t id_;
+    std::string text_;
+
+public:
+
+    Answer(uint16_t id, std::string text)
+    {
+        text_ = text;
+        id_ = id;
+    }
+
+    uint16_t GetId()
+    {
+        return id_;
+    }
+    std::string GetText()
+    {
+        return text_;
+    }
+
+
+
+    void SetId(uint16_t id)
+    {
+        id_ = id;
+    }
+    void SetText(std::string text)
+    {
+        text_ = text;
+    }
+};
+
+#endif
--- engine/objects/frame.h
+++ engine/objects/frame.h
@@ -1,9 +1,57 @@
 #ifndef FENGINE_OBJECTS_FRAME_H_
 #define FENGINE_OBJECTS_FRAME_H_
 
+#include "iostream"
+#include <vector>
+#include "answer.h"
+
 class Frame
 {
-    
+    private:
+
+    uint16_t id_;
+    std::string text_;
+    std::vector<Answer> answers_;
+
+    public:
+
+    Frame(uint16_t id, std::string text, std::vector<Answer> answers)
+    {
+        id_ = id;
+        text_ = text;
+        answers_ = answers;
+
+    }
+
+
+    uint16_t GetId()
+    {
+        return id_;
+    }
+    std::string GetText()
+    {
+        return text_;
+    }
+    std::vector<Answer> GetAnswers()
+    {
+        return answers_;
+    }
+
+
+    void SetId(uint16_t id)
+    {
+        id_ = id;
+    }
+    void SetText(std::string text)
+    {
+        text_ = text;
+    }
+    void SetAnswers(std::vector<Answer> answers)
+    {
+        answers_ = answers;
+    }
+
+
 };
 
 #endif
