478ce97a7b140353fca8c470cf234d2cb5a5f0d6
rdavid22 <raczdwork@gmail.com>
2022-12-13 19:10:03 +0100

    Upload

--- engine/game.h
+++ engine/game.h
@@ -10,37 +10,47 @@
 class Game
 {
 private:
+    Player current_player_;
     std::vector<Frame> all_frames_;
     std::vector<Player> all_players_;
     bool StartGame = false;
     bool ExitGame = false;
+
 public:
     void LoadPlayers()
     {
-
+        all_players_ = ReadPlayers();
     }
 
     void LoadFrames()
     {
-
+        all_frames_ = ReadFrames();
     }
 
     void LoadMenu()
     {
-        while(!StartGame)
+        while (!StartGame)
         {
-            
         }
     }
 
     void Start()
     {
-        while(!ExitGame)
+        while (!ExitGame)
         {
-
         }
     }
 
+    Frame GetFrameById(uint16_t id_to_get)
+    {
+        for(auto frame : all_frames_)
+        {
+            if(id_to_get == frame.GetId())
+            {
+                return frame;
+            }
+        }
+    }
 };
 
 #endif
---
+++ engine/menu.h
@@ -0,0 +1,9 @@
+#ifndef FENGINE_MENU_H_
+#define FENGINE_MENU_H_
+
+class Menu
+{
+
+};
+
+#endif
