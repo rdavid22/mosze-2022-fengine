fca17c3adda68c17a4278abfe4d7304e5b16c817
gergohaszonits <haszonits.gergo@gmail.com>
2022-12-13 23:58:01 +0100

    system call remove

--- engine/helper.h
+++ engine/helper.h
@@ -3,7 +3,9 @@
 
 #include "vector"
 #include "iostream"
-
+#if _WIN32
+#include <windows.h>
+#endif
 std::vector<std::string> Split(std::string text, std::string delimiter)
 {
     std::vector<std::string> parts;
@@ -35,14 +37,122 @@ std::string Trim(std::string text)
     }
     return out;
 }
-bool StartsWith(std::string text,std::string part)
+bool StartsWith(std::string text, std::string part)
 {
-    if(part.length() > text.length()){return false;}
-    for(uint16_t i = 0;i< part.length();i++)
+    if (part.length() > text.length())
     {
-        if(part[i] != text[i]){return false;}
+        return false;
+    }
+    for (uint16_t i = 0; i < part.length(); i++)
+    {
+        if (part[i] != text[i])
+        {
+            return false;
+        }
     }
     return true;
+}
+void PlaySound()
+{
+#if _WIN32
+
+    Beep(262, 800);
+    Beep(262, 400);
+    Beep(262, 600);
+    Beep(262, 1600);
+    Beep(262, 400);
+    Beep(294, 800);
+    Beep(294, 800);
+    Beep(330, 400);
+    Beep(330, 600);
+    Beep(330, 1600);
+    Beep(330, 400);
+    Beep(330, 800);
+    Beep(330, 800);
+    Beep(330, 400);
+    Beep(349, 800);
+    Beep(349, 800);
+    Beep(349, 400);
+    Beep(349, 800);
+    Beep(392, 800);
+    Beep(392, 400);
+    Beep(392, 800);
+    Beep(392, 800);
+    Beep(392, 200);
+    Beep(392, 400);
+    Beep(392, 600);
+    Beep(392, 1600);
+    Beep(392, 400);
+    Beep(392, 800);
+    Beep(392, 800);
+    Beep(392, 400);
+    Beep(392, 600);
+    Beep(392, 1600);
+    Beep(392, 400);
+    Beep(440, 800);
+    Beep(440, 800);
+    Beep(440, 400);
+    Beep(440, 600);
+    Beep(440, 1600);
+    Beep(440, 400);
+    Beep(440, 600);
+    Beep(440, 1600);
+    Beep(440, 400);
+    Beep(440, 600);
+    Beep(440, 1600);
+    Beep(440, 800);
+    Beep(440, 800);
+    Beep(440, 800);
+    Beep(440, 800);
+    Beep(440, 200);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 300);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 200);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 800);
+    Beep(494, 300);
+    Beep(494, 800);
+    Beep(523, 800);
+    Beep(523, 800);
+    Beep(523, 800);
+    Beep(523, 800);
+    Beep(523, 400);
+    Beep(523, 600);
+    Beep(523, 1600);
+    Beep(523, 400);
+    Beep(523, 600);
+    Beep(523, 1600);
+    Beep(523, 400);
+    Beep(523, 800);
+    Beep(523, 800);
+    Beep(523, 200);
+    Beep(523, 200);
+    Beep(523, 800);
+    Beep(523, 800);
+    Beep(587, 800);
+    Beep(587, 800);
+    Beep(587, 300);
+    Beep(587, 800);
+    Beep(587, 200);
+    Beep(587, 200);
+    Beep(587, 800);
+    Beep(587, 800);
+    Beep(659, 800);
+    Beep(659, 800);
+    Beep(659, 300);
+    Beep(659, 800);
+
+#endif
 }
 
 #endif
